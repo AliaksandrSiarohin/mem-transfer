5c65e8f8010f04ea56f1ad14aa8e7f5a2d7aa519
AliaksandrSiarohin <Aliaksandr.Siarohin@gmail.com>
2018-02-14 17:45:16 +0100

    Update train_models.sh

--- texture_nets/train_models.sh
+++ texture_nets/train_models.sh
@@ -5,7 +5,7 @@ textures=$(ls $texture_folder)
 models_folder=data/checkpoints/$style_weight/$dataset
 results_folder=datasets/result/$style_weight/$dataset
 input_folder=datasets/"$dataset"_test
-mem_score_file=$1_$2_memorability_external.csv
+mem_score_file=$1_$2_memorability_internal.csv
 rm log.out.txt, log.err.txt
 mkdir -p $results_folder
 mkdir -p $models_folder
@@ -16,21 +16,17 @@ do
 	echo $progress/$(echo $textures|wc -w|cut -f1)
 	progress=$(($progress + 1))
 	mkdir -p $models_folder/$file
-	#th train.lua -data datasets/lamem -style_image $texture_folder/$file \
-	# -style_size 256 -image_size 256 -model johnson -batch_size 4 -learning_rate 1e-2 -style_weight $style_weight \
-	# -style_layers relu1_2,relu2_2,relu3_2,relu4_2 -content_layers relu4_2 \
-	# -backend nn -checkpoints_path $models_folder/$file -num_iterations 10000 >> log.out.txt 2>> log.err.txt
+	th train.lua -data datasets/lamem -style_image $texture_folder/$file \
+	 -style_size 256 -image_size 256 -model johnson -batch_size 4 -learning_rate 1e-2 -style_weight $style_weight \
+	 -style_layers relu1_2,relu2_2,relu3_2,relu4_2 -content_layers relu4_2 \
+	 -backend nn -checkpoints_path $models_folder/$file -num_iterations 10000 >> log.out.txt 2>> log.err.txt
 	mkdir -p $results_folder/$file
 	
 
-#	th test_dir.lua -image_size 256 -input_path $input_folder -model_t7 $models_folder/$file/model_10000.t7 -save_path $results_folder/$file >> log.out.txt 2>> log.err.txt
+	th test_dir.lua -image_size 256 -input_path $input_folder -model_t7 $models_folder/$file/model_10000.t7 -save_path $results_folder/$file >> log.out.txt 2>> log.err.txt
 
 
 	th eval.lua -input_file ../lamem/lamem/splits/test_1.txt -seed_name $file -image_dir $results_folder/$file/ -output_file $mem_score_file >> log.out.txt 2>> log.err.txt
 
-	#python mem_rank.py $input_folder $texture_folder/$file $results_folder/$file $mem_score_file >> log.out.txt 2>> log.err.txt
-
 done
 python memorability_file_format.py $mem_score_file "$dataset"_external_sw"$style_weight".txt
-#echo "Ploting..."
-#python plot_results.py $mem_score_file $results_folder/good $results_folder/bad >> log.out.txt 2>> log.err.txt
